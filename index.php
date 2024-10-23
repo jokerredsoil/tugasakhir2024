0f27a7ced331676e2c90dd679f5e2042f2e53534
jokerredsoil <septianxnugraha@gmail.com>
2024-10-23 15:07:34 +0700

    finish footer header, add index.php

--- index.php
+++ index.php
@@ -1,70 +1,30 @@
 <?php
-require 'connection.php';
 
-
-// session_start();
-
-// if(!isset($_SESSION['username'])){
-//     header("Location: login.php");
-//     exit();
-// }
-
-$data_karyawan  = myquery("SELECT * FROM tbl_karyawan ");
-
-
-
-
-
-
+include('layout/header.php')
 ?>
+<main>
+    <div class="container-sm">
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
 
+    </div>
 
-<!DOCTYPE html>
-<html lang="en">
+</main>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-
-    <table class="table" border='1'>
-        <h3><a href="add.php">tambah data</a></h3>
-        <thead>
-            <tr>
-                <th>nik</th>
-                <th>nama karyawan</th>
-                <th>tanggal masuk</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($data_karyawan as $data): ?>
-                <tr>
-                    <td><?= $data['nik'] ?></td>
-                    <td><?= $data['nama_karyawan'] ?></td>
-                    <td><?= $data['tanggal_masuk'] ?></td>
-                    <td scope="row">
-
-
-                        <a href="form_edit.php?id=<?= $data['id'] ?>" class="btn btn-primary">Edit</a>
-                  
-                       
-                       <a href="functions.php?action=delete&id=<?= $data['id'] ?>" class="btn btn-outline-danger" onClick="return confirm('Yakin akan menghapus?')">Hapus</a>
-
-                    </td>
-
-                </tr>
-
-            <?php endforeach; ?>
-        </tbody>
-
-    </table>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php
+include('layout/footer.php')
+?>
