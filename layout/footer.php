496dfe656187f74284f3b0b724470d0d2ef4bb13
jokerredsoil <septianxnugraha@gmail.com>
2024-10-24 07:03:00 +0700

    merge index footer header

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,7 @@
-<footer>
-        <h3> made by &copy Septian Nugraha</h3>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<footer class="bg-dark text-white text-center py-0.2 mt-auto">
+    made by &copy Septian Nugraha
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
